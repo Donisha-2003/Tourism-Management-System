e2b2026378626cb974c8df34655b833c283036e3
Donisha-2003 <143182414+Donisha-2003@users.noreply.github.com>
2023-10-25 18:19:27 +0530

    includes

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+
+
+
+
+<!--- /footer-top ---->
+
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons ">
+			<ul>
+				<li><a class="facebook" href="#"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="#"><span>Twitter</span></a></li>
+				<li><a class="googleplus" href="#"><span>Google+</span></a></li>
+				
+			</ul>
+		</div>
+		<p>   TMS |  Dept of computer Application </p>
+	</div>
+</div>
